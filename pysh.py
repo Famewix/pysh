7214591357dd390a6a6c66414b954a963e4db11b
Faridin Islam <faridin05saif@gmail.com>
2022-05-08 00:16:30 +0600

    uwu

--- pysh.py
+++ pysh.py
@@ -4,17 +4,29 @@ import sys
 import getpass
 import subprocess
 
+def get_current_dir():
+    if os.getcwd() == os.path.expanduser('~'):
+        return "~"
+    else:
+        return os.getcwd()
+
 USERNAME = getpass.getuser()
 HOSTNAME = os.uname()[1]
-DIR = os.getcwd()
+DIR = get_current_dir()
 
-PS=f"{USERNAME}@{HOSTNAME}:{DIR} # "
 
 while True:
     try:
+        PS=f"{USERNAME}@{HOSTNAME}:{DIR} # "
         cmd_input = input(PS)
         if cmd_input == "exit":
             sys.exit(0)
+        elif cmd_input.split(" ")[0] == "cd":
+            if cmd_input == "cd":
+                os.chdir(os.path.expanduser('~'))
+            else:
+                os.chdir(cmd_input.split(' ')[-1])
+            DIR = get_current_dir()
         os.system(cmd_input)
     except KeyboardInterrupt:
         print("^C")
